cfd19a20249a1bfac6e28e77c8d2c69fd048ba7c
Omar Ahmed <omarsomaa55@gmail.com>
2025-11-20 15:20:53 +0200

    solve mini-conf

--- apps/solidarity/api/views.py
+++ apps/solidarity/api/views.py
@@ -1,6 +1,5 @@
 from django.db import DatabaseError
 from django.http import HttpResponse
-from django.db import DatabaseError
 from django.template.loader import render_to_string
 import io as io
 import asyncio
