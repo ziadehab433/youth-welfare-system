49d7607b42de592b7a0b6b180147dfb97ea97ded
ziad ehab <ziadelsherif7@gmail.com>
2025-11-21 15:26:12 +0200

    deleted unused files

--- apps/solidarity/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Solidarities  # adjust name based on your table
-
-class SolidaritySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Solidarities
-        fields = '__all__'  # or list specific fields
