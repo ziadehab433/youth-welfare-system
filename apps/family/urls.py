922248348e45100832cd0e7d4e2b86f31c8f3f01
Omar Ahmed <omarsomaa55@gmail.com>
2025-12-06 21:06:07 +0200

    fix naming rout path for fam faculty

--- apps/family/urls.py
+++ apps/family/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from apps.family.views.faculty import FamilyFacultyAdminViewSet
 
 router = DefaultRouter()
-router.register(r'family', FamilyFacultyAdminViewSet, basename='family-admin')
+router.register(r'faculty', FamilyFacultyAdminViewSet, basename='family-admin')
 
 urlpatterns = [
     path('family/', include(router.urls)),
