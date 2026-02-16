9f7558d484063db99773b2c85bd3663d1968600a
Omar Ahmed <omarsomaa55@gmail.com>
2026-02-16 22:37:17 +0200

    fix urls merge conflict

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -1,27 +1,19 @@
 from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('faculty/plans/<int:plan_id>/export-pdf/', views.export_plan_pdf, name='plan_pdf'),
-
-
-
-urlpatterns = [
-    path('events/', include('apps.event.plans.urls')),
-  
 from rest_framework.routers import DefaultRouter
-from apps.event.events.views import EventManagementViewSet , EventARViewSet, EventActivationViewSet, EventGetterViewSet
+from apps.event.events.views import (
+    EventManagementViewSet,
+    EventARViewSet,
+    EventActivationViewSet,
+    EventGetterViewSet
+)
 
 router = DefaultRouter()
-
 router.register(r'manage-events', EventManagementViewSet, basename='faculty_events')
 router.register(r'approve-events', EventARViewSet, basename='admin_events')
-router.register(r'get-events', EventGetterViewSet, basename='event_getter') 
+router.register(r'get-events', EventGetterViewSet, basename='event_getter')
 router.register(r'activate-events', EventActivationViewSet, basename='event_activation')
 
 urlpatterns = [
-    path('event/', include(router.urls)),
-]
-urlpatterns = [
+    path('events/', include('apps.event.plans.urls')),
     path('event/', include(router.urls)),
 ]
