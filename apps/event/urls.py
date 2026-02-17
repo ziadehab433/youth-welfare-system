1152a9117ce252a87133f01bdde1aee1e2121f85
Ahmedabdulaleem <135714204+Ahmedabdulaleem@users.noreply.github.com>
2026-02-17 21:43:03 +0200

    complete event participant management with approve/reject/assign-result

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from apps.event.events.participants_management_views import EventParticipantViewSet
 from apps.event.events.event_management_views import (
     EventManagementViewSet,
     EventARViewSet,
@@ -14,7 +15,7 @@ router.register(r'approve-events', EventARViewSet, basename='admin_events')
 router.register(r'get-events', EventGetterViewSet, basename='event_getter')
 router.register(r'activate-events', EventActivationViewSet, basename='event_activation')
 router.register(r'student-events', StudentEventViewSet, basename='student_events')
-
+router.register(r'manage-participants', EventParticipantViewSet, basename='manage_participants')
 urlpatterns = [
     path('events/', include('apps.event.plans.urls')),
     path('event/', include(router.urls)),
